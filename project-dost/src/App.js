defce1a61e329a68f6fb0f2e8cca06ac99c68e4f
= <=>
2021-03-18 18:46:41 -0700

    Set up

---
+++ project-dost/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Main from 'pages/main';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ project-dost/src/pages/main/index.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import Profile from 'comps/profile';
+
+
+const Main = () => {
+
+    return <div className="main">
+        Test
+    </div>
+}
+
+export default Main;
