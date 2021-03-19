a3f35aee7dc3a10631b7bb3b740134f1aa5ae849
= <=>
2021-03-18 21:06:29 -0700

    componentssssss

--- project-dost/src/pages/main/index.js
+++ project-dost/src/pages/main/index.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react';
 import Profile from 'comps/profile';
+import Avatar from 'comps/avatar';
+import Timeline from 'comps/timeline';
 
 
 const Main = () => {
 
     return <div className="main">
         <Profile />
+        <Timeline />
     </div>
 }
 
